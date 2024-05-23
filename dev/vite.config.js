afaf656e4f59424fa01c37d4d938bd48250da49f
Dmytro Sholka <dmytro.sholka@ministrybrands.com>
2024-05-22 20:06:15 -0400

    updated

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -10,20 +10,12 @@ const buildTempDir = fileURLToPath(new URL('../temp', import.meta.url))
 
 async function copyFiles() {
   const filesToCopy = ['favicon.ico', 'index.html']
-  // const assetPrefix = 'index-'
   const assetsDir = path.join(outputDir, 'assets')
 
   try {
     if (await fs.pathExists(assetsDir)) {
       await fs.remove(assetsDir)
     }
-
-    // const oldAssets = await fs.readdir(outputDir)
-    // for (const file of oldAssets) {
-    //   if (file.startsWith(assetPrefix) && (file.endsWith('.js') || file.endsWith('.css'))) {
-    //     await fs.remove(path.join(outputDir, file))
-    //   }
-    // }
 
     for (const file of filesToCopy) {
       const srcPath = path.join(buildTempDir, file)
@@ -41,14 +33,6 @@ async function copyFiles() {
     } else {
       console.log('Assets directory not found');
     }
-
-    // const assetsSrcPath = path.join(buildTempDir, 'assets')
-    // const assetsDestPath = path.join(outputDir, 'assets')
-    // if (await fs.pathExists(assetsSrcPath)) {
-    //   await fs.copy(assetsSrcPath, assetsDestPath)
-    // } else {
-    //   console.error('Error')
-    // }
 
     await fs.remove(buildTempDir)
   } catch (error) {
